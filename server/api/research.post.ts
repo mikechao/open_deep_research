06f847b71ab9a2cbcf8847cfd8b29e697adc0b58
mikechao <mike.chao.one@gmail.com>
2025-03-26 15:54:57 -0700

    Added research post endpoint and nuxi to debug

---
+++ server/api/research.post.ts
@@ -0,0 +1,30 @@
+import { Command } from '@langchain/langgraph'
+import { consola } from 'consola'
+import { z } from 'zod'
+import { graph } from '../graph/graph'
+
+export default defineLazyEventHandler(async () => {
+  const inputSchema = z.object({
+    topic: z.string(),
+    sessionId: z.string(),
+    feedBack: z.string().optional().default(''),
+  })
+  return defineEventHandler(async (event) => {
+    const body = await readBody(event)
+    const parsedBody = inputSchema.safeParse(body)
+    if (!parsedBody.success) {
+      const formattedError = parsedBody.error.flatten()
+      consola.error({ tag: 'eventHandler', message: `Invalid input: ${JSON.stringify(formattedError)}` })
+      throw createError({
+        statusCode: 400,
+        statusMessage: 'Bad Request',
+        message: JSON.stringify(formattedError) || 'Invalid input',
+      })
+    }
+    const { topic, sessionId, feedBack } = parsedBody.data
+    const threadConfig = { configurable: { thread_id: sessionId } }
+    const input = feedBack.length > 0 ? new Command({ resume: feedBack }) : { topic }
+    const result = await graph.invoke(input, threadConfig)
+    return result
+  })
+})
